e755f316fa8c074738d1ef0e6cdf72cd5873e241
Benjamin Batya <benjaminbatya@gmail.com>
2021-02-22 00:17:50 -0800

    Updated params

--- sample_params.sh
+++ sample_params.sh
@@ -2,4 +2,7 @@
 
 # python -m tf_bodypix replace-background --source /dev/video0 --mask-mean-count=2 --mask-blur=10 --threshold=0.65 --background ~/Documents/art/beach.webp --output /dev/video4
 
-python -m tf_bodypix auto-track --source /dev/video0 --mask-mean-count=2 --mask-blur=10 --threshold=0.65 --output /dev/video4
+python -m tf_bodypix auto-track --source /dev/video0 --mask-mean-count=2 --mask-blur=10 --threshold=0.65 \
+--crops-mean-count=8 --padding=50 --output /dev/video4
+
+# --source-width=1280 --source-height=960 
